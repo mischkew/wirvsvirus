185954eac642f398b6bb16e142176f8ccd644f8d
Sven Mkw <sven.mkw@gmail.com>
2020-03-22 19:21:30 +0100

    Updates

---
+++ precache-manifest.efac00125ef022faf2e5c636b860ec01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b45c5fb1b60bfce0b957381340a8e3",
+    "url": "/wirvsvirus/1cdd0fca5422b996813c.worker.js"
+  },
+  {
+    "revision": "d6e5e8e94a69b484c98354b33553c387",
+    "url": "/wirvsvirus/index.html"
+  },
+  {
+    "revision": "f18d1355d10b07d0410a",
+    "url": "/wirvsvirus/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c93780ab47012b263352",
+    "url": "/wirvsvirus/static/css/main.9971ae72.chunk.css"
+  },
+  {
+    "revision": "f18d1355d10b07d0410a",
+    "url": "/wirvsvirus/static/js/2.62e93b49.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/wirvsvirus/static/js/2.62e93b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93780ab47012b263352",
+    "url": "/wirvsvirus/static/js/main.8dfcacda.chunk.js"
+  },
+  {
+    "revision": "56bbea15ffd03810f176",
+    "url": "/wirvsvirus/static/js/runtime-main.dbac568a.js"
+  }
+]);
