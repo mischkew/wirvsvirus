cd18e2b8abb028bd3ca5cdd3501e41989a3c9b5c
Sven Mkw <sven.mkw@gmail.com>
2020-03-22 19:15:25 +0100

    Create static build at /docs for gh-pages

---
+++ docs/precache-manifest.45a6cc4fe21286d4e032b713b690c45a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e2669b607fe1e69fd9f83b937d1de1",
+    "url": "/3e93649547b1e5ff1b78.worker.js"
+  },
+  {
+    "revision": "c3ecbbc8763829894c2d9b5d892976b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18d1355d10b07d0410a",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "73bd2848daff2cefa066",
+    "url": "/static/css/main.9971ae72.chunk.css"
+  },
+  {
+    "revision": "f18d1355d10b07d0410a",
+    "url": "/static/js/2.62e93b49.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.62e93b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bd2848daff2cefa066",
+    "url": "/static/js/main.7b942d7b.chunk.js"
+  },
+  {
+    "revision": "79543179f9b09c979d0f",
+    "url": "/static/js/runtime-main.1b305e74.js"
+  }
+]);
