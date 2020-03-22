1f7712c06dbbfad7e1bdaa0b9a9e494757163743
fredchettouh <frederik@Frederiks-MBP.fritz.box>
2020-03-22 14:58:13 +0100

    prunning version 1

--- graph/street_graph.py
+++ graph/street_graph.py
@@ -2,6 +2,7 @@ import json
 
 import osmnx as ox
 from sklearn.neighbors import KDTree
+import numpy as np
 
 
 def get_central_node(graph, lat, long):
@@ -26,8 +27,44 @@ def get_neighbourhood(graph):
     lngs = [float(x) for _, x in graph.nodes(data="x")]
     return zip(lats, lngs)
 
+def prune_nodes(central_node,nodes, adj_list,lat_n_longs,n_paths=2, max_hops=3):
+    start = central_node
+    nodes_on_paths = []
+    
+    for i in range(n_paths):
+        current_paths=[]
+        hop = 0
+        hops = max_hops
+        current_node = start
+        start_index = nodes.index(start)
+        
+        print(list(adj_list[start_index][1].keys()))
+        if not len(list(adj_list[start_index][1].keys())):
+            print('No adjency')
+            continue
+            while hop <= hops:
+                node_index = nodes.index(current_node)
+                if not len(list(adj_list[node_index][1].keys())):
+                    break
+                else:
+                    next_node = np.random.choice(list(adj_list[node_index][1].keys()))
+                    current_paths.append(current_node)
+                    current_node = next_node
+                    hop = hop+1
+        nodes_on_paths.append(current_paths)
+    
+    set_path=set(inner for outer in nodes_on_paths for inner in outer)
+    
+    nodes_to_keep = [node for node in nodes if node in set_path]
+    nodes_to_keep_indexes = [nodes.index(node) for node in nodes_to_keep]
+    pruned_adj_list = [adj_list[index] for index in nodes_to_keep_indexes]
+    pruned_lat_n_longs = [lat_n_longs[index] for index in nodes_to_keep_indexes]
+    
+    return nodes_to_keep,pruned_adj_list,pruned_lat_n_longs
+    
 
-def process_graph(graph, data_dict):
+
+def process_graph(graph, data_dict,central_node,n_paths=2,max_hops=20):
     """process a single graph, getting all adjacent nodes and
     and add them to a the dictionary mapping the stations"""
 
@@ -35,19 +72,24 @@ def process_graph(graph, data_dict):
     zipper = get_neighbourhood(graph)
     nodes = list(graph.nodes)
     lat_n_longs = list(zipper)
+    adj_list = list(graph.adjacency())
+
+
+    nodes,adj_list,lat_n_longs=prune_nodes(central_node,nodes,adj_list,lat_n_longs,n_paths,max_hops)
 
     #     mapping adjacent streets
-    adj_list = list(graph.adjacency())
     for i, node in enumerate(nodes):
         data_dict["stations"][str(node)] = {}
         data_dict["stations"][str(node)]["type"] = "street"
         data_dict["stations"][str(node)]["lat"] = lat_n_longs[i][0]
         data_dict["stations"][str(node)]["lng"] = lat_n_longs[i][1]
         next_stops = list(map(str, adj_list[i][1].keys()))
-        data_dict["stations"][str(node)]["next_stops"] = next_stops
+        # data_dict["stations"][str(node)]["next_stops"] = next_stops
+        data_dict['stations'][str(node)]['next_stops'] = [stop for stop in next_stops if stop in nodes]
 
 
-def process_stations(data_dict, distance=300):
+
+def process_stations(data_dict, distance=300):  
     """processing all stations from the input file"""
     #     opening the json and converting to dict
     temp_dict = {"stations": {}}
@@ -66,7 +108,8 @@ def process_stations(data_dict, distance=300):
         central_node = get_central_node(graph, lat, long)
         data_dict["stations"][str(key)]["next_stops"].append(str(central_node))
 
-        process_graph(graph, temp_dict)
+        process_graph(graph, temp_dict,central_node)
+        print(list(temp_dict['stations'].keys()))
         temp_dict["stations"][str(central_node)]["next_stops"].append(str(key))
 
     data_dict["stations"].update(temp_dict["stations"])
